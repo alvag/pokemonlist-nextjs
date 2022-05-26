8174d7730562723d7fcfaaec4421b83b299f094d
Max Alva Garcia <maxrober.alvagarcia@everis.nttdata.com>
2022-05-25 21:16:30 -0500

    Incremental Static Regeneration

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -98,17 +98,30 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
     return {
         paths: pokemons.map( ( name ) => ( { params: { name } } ) ),
-        fallback: false
+        // fallback: false,
+        fallback: 'blocking'
     };
 };
 
 export const getStaticProps: GetStaticProps = async ( { params } ) => {
     const { name } = params as { name: string };
 
+    const pokemon = await getPokemonInfo( name );
+
+    if ( !pokemon ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        };
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo( name )
-        }
+            pokemon
+        },
+        revalidate: 86400
     };
 };
 
